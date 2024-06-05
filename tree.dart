35163530b7693b5c95f596f311603c54471ebd0f
DUDU0123 <sdu200115@gmail.com>
2024-06-05 13:54:51 +0530

    Small edit

--- tree.dart
+++ tree.dart
@@ -7,76 +7,95 @@ class BinaryTreeNode {
   BinaryTreeNode? rightNode;
 }
 
-
-class BinarySearchTree{
+class BinarySearchTree {
   BinaryTreeNode? root;
 
-  insert({required int data}){
+  insert({required int data}) {
     BinaryTreeNode? node = root;
-    if (node==null) {
+    if (node == null) {
       root = BinaryTreeNode(data: data);
+      return;
     }
 
-    while (node!=null) {
-      if (data<node.data!) {
-        if (node.leftNode==null) {
+    while (node != null) {
+      if (data < node.data!) {
+        if (node.leftNode == null) {
           node.leftNode = BinaryTreeNode(data: data);
           break;
-        }else{
+        } else {
           node = node.leftNode;
         }
-      }else if(data>node.data!){
-        if (node.rightNode==null) {
+      } else if (data > node.data!) {
+        if (node.rightNode == null) {
           node.rightNode = BinaryTreeNode(data: data);
           break;
-        }else{
+        } else {
           node = node.rightNode;
         }
       }
     }
   }
 
-
-  contains({required int data}){
+  contains({required int data}) {
     BinaryTreeNode? node = root;
-    while (node!=null) {
-      if (data<node.data!) {
+    while (node != null) {
+      if (data < node.data!) {
         node = node.leftNode;
-      }else if(data>node.data!){
+      } else if (data > node.data!) {
         node = node.rightNode;
-      }else{
+      } else {
         return true;
       }
     }
     return false;
   }
 
-  inOrder(){
+  inOrder() {
     print("InOrder");
     inOrderHelper(node: root);
   }
-  preOrder(){
+
+  preOrder() {
     print("PreOrder");
     preOrderHelper(node: root);
   }
-  postOrder(){
+
+  postOrder() {
     print("PostOrder");
     postOrderHelper(node: root);
   }
 
-  inOrderHelper({required BinaryTreeNode? node}){
-    inOrderHelper(node: node?.leftNode);
-    print(node?.data);
-    inOrderHelper(node: node?.rightNode);
+  inOrderHelper({required BinaryTreeNode? node}) {
+    if (node != null) {
+      inOrderHelper(node: node.leftNode);
+      print(node.data);
+      inOrderHelper(node: node.rightNode);
+    }
   }
-  preOrderHelper({required BinaryTreeNode? node}){
-    print(node?.data);
-    preOrderHelper(node: node?.leftNode);
-    preOrderHelper(node: node?.rightNode);
+
+  preOrderHelper({required BinaryTreeNode? node}) {
+    if (node != null) {
+      print(node.data);
+      preOrderHelper(node: node.leftNode);
+      preOrderHelper(node: node.rightNode);
+    }
   }
-  postOrderHelper({required BinaryTreeNode? node}){
-    postOrderHelper(node: node?.leftNode);
-    postOrderHelper(node: node?.rightNode);
-    print(node?.data);
+
+  postOrderHelper({required BinaryTreeNode? node}) {
+    if (node != null) {
+      postOrderHelper(node: node.leftNode);
+      postOrderHelper(node: node.rightNode);
+      print(node.data);
+    }
   }
+}
+
+void main() {
+  BinarySearchTree b = BinarySearchTree();
+  b.insert(data: 10);
+  b.insert(data: 1);
+  b.insert(data: 20);
+  b.insert(data: 30);
+  b.insert(data: 32);
+  b.inOrder();
 }
